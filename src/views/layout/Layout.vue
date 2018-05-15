c93780cc5dba81d3a31013ae5257c1f1748b7a9a
wangjingli <wangjingli@zillionfortune.com>
2018-05-15 19:01:09 +0800

    整体布局

--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -1,29 +1,33 @@
 <template>
   <div class="app-wrapper" :class="{hideSidebar:!sidebar.opened}">
-    <sidebar class="sidebar-container"></sidebar>
+    <!-- <sidebar class="sidebar-container"></sidebar> -->
     <div class="main-container">
       <navbar></navbar>
       <app-main></app-main>
     </div>
+    <footerbar></footerbar>
   </div>
 </template>
 
 <script>
-import { Navbar, Sidebar, AppMain } from '@/views/layout/components'
+//删掉sidebar：, Sidebar
+//
+import { Navbar, AppMain, Footerbar } from "@/views/layout/components";
 
 export default {
-  name: 'layout',
+  name: "layout",
   components: {
     Navbar,
-    Sidebar,
-    AppMain
+    // Sidebar,
+    AppMain,
+    Footerbar
   },
   computed: {
     sidebar() {
-      return this.$store.state.app.sidebar
+      return this.$store.state.app.sidebar;
     }
   }
-}
+};
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
@@ -33,5 +37,8 @@ export default {
   position: relative;
   height: 100%;
   width: 100%;
+}
+.main-container{
+  
 }
 </style>
---
+++ src/views/layout/components/Footerbar.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="page-footer">
+
+	</div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  watch: {},
+  computed: {},
+  methods: {}
+  //   created() {},
+  //   mounted() {},
+  //   destroyed() {}
+};
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+
+</style>
